f5fa68254df9fbb31c9d6587309252fd64112758
unknown <isak.nilsson.skola@gmail.com>
2023-04-12 17:24:26 +0200

    finished file reading

--- lab2.py
+++ lab2.py
@@ -13,8 +13,8 @@ t_string = t_string + "BalancedTree_3_5.td"
 g_file = open(g_string)
 t_file = open(t_string)
 
-n_e = 0
-n_v = 0
+n_e = 0 #number of edges in input
+n_v = 0 #number of vertices in input
 
 while True:
     l = g_file.readline()
@@ -52,7 +52,22 @@ for l in g_file:
         g[v2] = [v1]
 
 
-t = [] #what data struct for this?        
+t = {} #dict for edges in tree - Ill use one dict for bags and one for bag-edges (both with bag nbr as key) - could make joint data class
+bags = {} #dict for bags
+num_bags = 0
+tree_w = 0
+num_v = 0 #number of vertices in the original graph (should match n_v)
+  
+while True:
+    l = t_file.readline()
+    print("reading start: ", l)
+    syms = l.split()
+    if syms[0] == 's':
+        num_bags = int(syms[2])
+        tree_w = int(syms[3])-1
+        num_v = int(syms[4])
+        
+        break     
         
 for l in t_file:
     
@@ -65,12 +80,24 @@ for l in t_file:
         bag_n = int(vertices[1])
         vertices = vertices[2:len(vertices)] #remove first 2 (symbol + bag nbr), rest should be nodes
         vertices = [int(v) for v in vertices]
-        bag = []
-        for v in vertices: #bag stuff
-            bag.append(v)
-    else:
+        bags[bag_n] = vertices
+        
+    else: #in this case we have rows describing edges, so 2 values
         #do edge stuff
-        continue
+        v1 = int(vertices[0])
+        #if v1 == 1:
+         #   print(g[v1])
+        v2 = int(vertices[1])
+        
+        if t.get(v1): #if already in dict
+            t[v1].append(v2)
+        else:
+            t[v1] = [v2]
+        if t.get(v2): #if already in dict
+            t[v2].append(v1)
+        else:
+            t[v2] = [v1]
+        
         
             
             
