1f7808da17e4bf3ce1f6dbb31633adb7b99bb70f
Eliisaaaa <faureelisa@free.fr>
2023-05-15 18:42:01 +0200

    montecarlo update

--- lab4/montecarlo.py
+++ lab4/montecarlo.py
@@ -10,10 +10,6 @@ def montecarlo_rec(N,H,position, proba_matrix, time_matrix, time=0):
         return montecarlo_rec(N, H,chosen_option, proba_matrix, time_matrix, time=0)
     
 def montecarlo(N,H,F,P, proba_matrix, time_matrix):
-    temps_f = 0
-    position_f = F
-    temps_p = 0
-    position_p = P
     time_f = montecarlo_rec(N,H,F, proba_matrix, time_matrix)
     time_p = montecarlo_rec(N,H,P, proba_matrix, time_matrix)
     return time_f, time_p 
