fb864c35f5060fb9b0b7038acf20ad2c95cce8f5
Eliisaaaa <faureelisa@free.fr>
2023-05-15 19:07:14 +0200

    update

--- lab4/montecarlo.py
+++ lab4/montecarlo.py
@@ -1,11 +1,10 @@
 import random
 
-    
 def montecarlo_rec(N,H,position, proba_matrix, time_matrix, time=0):
     if position == H:
         return time
     else :
-        chosen_option = random.choices([i for i in range(N) if proba_matrix[position][i]!=0], proba_matric[position], k=1)
+        chosen_option = random.choices([i for i in range(N) if proba_matrix[position][i]!=0], proba_matrix[position], k=1)
         time += time_matrix[position][chosen_option] 
         return montecarlo_rec(N, H,chosen_option, proba_matrix, time_matrix, time=0)
     
@@ -13,3 +12,7 @@ def montecarlo(N,H,F,P, proba_matrix, time_matrix):
     time_f = montecarlo_rec(N,H,F, proba_matrix, time_matrix)
     time_p = montecarlo_rec(N,H,P, proba_matrix, time_matrix)
     return time_f, time_p 
+
+A, b, N, M, H, F, P = read_file(datapath)
+montecarlo(N,H,F,P, A, time_matrix)
+
